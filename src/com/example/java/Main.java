bc1f65f9a9c928d63664c5de4bc0b21283f3b1d6
stesho01 <Listo4ek>
2022-01-05 19:52:27 +0200

    change array list initialization

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -6,15 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Convert conv = new Convert();
-        ArrayList<String> alist = new ArrayList<>();
-        alist.add("AA");
-        alist.add("AA");
-        alist.add("BB");
-        alist.add("BB");
-        alist.add("CC");
-        alist.add("CC");
-        alist.add("AA");
-        alist.add("BB");
+        ArrayList<String> alist = new ArrayList<>(Arrays.asList("AA", "BB", "CC","DD","DD", "AA", "BB","CC"));
         System.out.println("Collection with duplicates contains " + alist);
         HashSet<String> newcol = conv.convert(alist);
         System.out.println("Collection with duplicates contains " + newcol);
