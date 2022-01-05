d5c2c2d960c2c2602d63aedd2d3139531d6b4485
stesho01 <Listo4ek>
2022-01-05 15:51:27 +0200

    fix task

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -1,16 +1,26 @@
 package com.example.java;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        String arr [] = {"AA", "BB", "CC", "CC", "BB", "AA", "NN"};
-        Set<String> set;
-        Collections.addAll(set = new HashSet<String>(Arrays.asList((arr))));
-        System.out.println("Collection without duplicates contains" + set);
+
+        ArrayList<String> alist = new ArrayList<>();
+        alist.add("AA");
+        alist.add("AA");
+        alist.add("BB");
+        alist.add("BB");
+        alist.add("CC");
+        alist.add("CC");
+
+        HashSet<String> hset = new HashSet<>();
+        for(String str :alist){
+           hset.add(str);
+        }
+     //    Set<String> set;
+     //   Collections.addAll(set = new HashSet<String>(Arrays.asList((arr))));
+        System.out.println("Collection with duplicates contains " + alist);
+        System.out.println("Collection without duplicates contains " + hset);
     }
 }
